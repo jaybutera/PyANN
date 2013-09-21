9086177ec05722b51548e3b4e29b1e76e947e431
Jay Butera <buterajay@gmail.com>
2013-09-21 00:53:12 -0500

    Cleaned backPropogation test for first functional version

--- nn/backPropagation.py
+++ nn/backPropagation.py
@@ -1,5 +1,6 @@
 from graph import *
 import matplotlib.pyplot as plt
+from random import random
 
 # Evenutally merge these two update weight functions to work with both output
 # and hidden nodes.
@@ -15,45 +16,43 @@ def backProp_test():
     Tests the back propagation algorithm with an arbitrary data set
     """
 
+    # Interestingly, the NN convergers when the value is under 1. However,
+    # occasionaly it will diverge and appproach positive or negative infinity.
+
     g = Graph()
     outputs = []
-    target = 2.
+    target = .2
     alpha = .01
-    MAX = 100
+    MAX = 1000
 
     inputs = [g.addNode(i, kin='input', ix = float(i+1)) for i in range(3)]
     hidden = [g.addNode(i) for i in range(3,5)]
     output = g.addNode(5, kin='output')
 
     # To hidden
-    g.addEdge(3,0,weight=.3)
-    g.addEdge(4,0,weight=.6)
-    g.addEdge(4,1,weight=.4)
-    g.addEdge(4,2,weight=.2)
-    g.addEdge(3,1,weight=.7)
-    g.addEdge(3,2,weight=.4)
+    g.addEdge(3,0,weight=round(random(),2))
+    g.addEdge(4,0,weight=round(random(),2))
+    g.addEdge(4,1,weight=round(random(),2))
+    g.addEdge(4,2,weight=round(random(),2))
+    g.addEdge(3,1,weight=round(random(),2))
+    g.addEdge(3,2,weight=round(random(),2))
 
     # To output
-    g.addEdge(5,3,weight=.5)
-    g.addEdge(5,4,weight=.8)
+    g.addEdge(5,3,weight=round(random(),2))
+    g.addEdge(5,4,weight=round(random(),2))
 
     # I hope you're ashamed of yourself. Shun yourself for the ugly code that
     # is below this comment
     di = [0 for i in range(len(hidden))]
 
     for i in range(MAX):
-        j=0
-        for outputWeight in output.getConnections():
-            di[j] = output.getChange(target)
+        for idx, outputWeight in enumerate(output.getConnections()):
+            di[idx] = output.getChange(target)
             updateOutputWeight(output, outputWeight, target, alpha)
-            #print di
-            j+=1
 
-        j=0
-        for hiddenNode in hidden:
+        for idx, hiddenNode in enumerate(hidden):
             for inputWeight in hiddenNode.getConnections():
-                updateHiddenWeight(hiddenNode, inputWeight, di[j], alpha)
-            j+=1
+                updateHiddenWeight(hiddenNode, inputWeight, di[idx], alpha)
 
         print 'output:', output.activate()
         outputs.append(output.activate())
@@ -64,6 +63,7 @@ def backProp_test():
     plt.title('ANN Output Value Evolution')
     plt.ylabel('Output value')
     plt.xlabel('Epoch')
+    plt.ylim(0,10)
 
     # Plot the output node at each epoch
     plt.plot(range(MAX), outputs, 'b-')
