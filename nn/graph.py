19ea7c53bf67c7f32d3732b40090a385ec42aa34
Jay Butera <buterajay@gmail.com>
2013-09-22 18:11:19 -0500

    Added comments

--- nn/graph.py
+++ nn/graph.py
@@ -6,12 +6,21 @@ class Graph:
         self.numNode = 0
 
     def addNode(self, key, kin='hidden', ix=0):
+        """
+        Adds a node of any type with no initial
+        connections to the graph.
+        """
         self.numNode = self.numNode + 1
         newNode = Node(key, kind=kin, i=ix)
         self.nodeList[key] = newNode
         return newNode
 
     def getNode(self, n):
+        """
+        Returns the instance of the specified
+        node ID. If the node does not exist,
+        returns None.
+        """
         if n in self.vertlist:
             return self.verList[n]
         else:
@@ -21,6 +30,12 @@ class Graph:
         return n in self.nodeList
 
     def addEdge(self, a, b, weight=0):
+        """
+        Adds an undirected edge of a specified
+        weight between node IDs a and b. If a
+        or b does not exist, they will be initialed
+        prior to edge placement.
+        """
         if a not in self.nodeList:
             self.addNode(a)
         if b not in self.nodeList:
@@ -29,17 +44,20 @@ class Graph:
         self.nodeList[a].addConnection(self.nodeList[b], weight)
 
     def getNodes(self):
+        """
+        Returns a list of all node IDs that
+        exist in the graph.
+        """
         return self.nodeList.keys()
 
     def __iter__(self):
         return iter(self.nodeList.values())
 
-"""
 def graph_test():
-    ""
+    """
     Tests the functionality of the fundamental components of the
     Graph class.
-    ""
+    """
     g = Graph()
 
     for i in range(6):
@@ -57,9 +75,9 @@ def graph_test():
             print("( %s , %s )" % (v.getId(), w.getId()))
 
 def activation_test():
-    ""
+    """
     Test the activation function for the Node class
-    ""
+    """
 
     g = Graph()
 
@@ -77,4 +95,3 @@ def activation_test():
     print [node.activate() for node in g]
     print [node.dActivate() for node in g]
 
-"""
--- nn/node.py
+++ nn/node.py
@@ -10,18 +10,34 @@ class Node:
         self.connectedTo = {}
 
     def getChange(self, target=None):
+        """
+        Returns the change in weight values.
+        """
         if self.kind == 'output':
             return (target-self.activate()) * self.dActivate()
         elif self.kind == 'hidden':
             return self.dActivate() * sum([self.getWeight(i)*self.di for i in self.getConnections()])
 
     def getDi(self):
+        """
+        Returns the node's deltaI value from the
+        previous weight update.
+        """
         return self.di
 
     def setDi(self, _di):
+        """
+        Set the error of the node's output times
+        the derivative of the activation function.
+        Referred to as deltaI.
+        """
         self.di = _di
 
     def addConnection(self, nbr, weight=0.):
+        """
+        Connects a separate node instance of a specified
+        weight (default 0.0) to the node.
+        """
         self.connectedTo[nbr] = weight
 
     def __str__(self):
@@ -29,29 +45,46 @@ class Node:
             self.connectedTo])
 
     def getConnections(self):
+        """
+        Returns a list of all node instances
+        that are connected to it.
+        """
         return self.connectedTo.keys()
 
     def getId(self):
+        """
+        Returns node ID.
+        """
         return self.id
 
     def getWeight(self, node):
+        """
+        Returns the weight between itself
+        and the specified node.
+        """
         return self.connectedTo[node]
 
     def setWeight(self, node, weight):
+        """
+        Sets the weight between itself
+        and the specified node.
+        """
         self.connectedTo[node] = weight
 
     def sigmoid(self, a):
+        """
+        Returns the sigmoid function mappng of
+        the specified input. Serves as a form
+        of activation function.
+        """
         return (1/(1+math.exp(-a)))
 
-    def dActivate(self):
-        if self.kind== 'input':
-            return self.val
-#        elif self.kind == 'hidden':
-        else:
-            x = self.activate()
-            return (x * (1-x))
-
     def activate(self):
+        """
+        Returns the value of the sum of connected
+        nodes' output values * the corresponding
+        weights fed into the sigmoid function.
+        """
         if self.kind == 'input':
             return self.val
         elif self.kind == 'hidden':
@@ -68,4 +101,16 @@ class Node:
             return a
         else:
             return None
+
+    def dActivate(self):
+        """
+        Returns the derivative of the activation
+        function. If the node is of type input,
+        the value remains the same.
+        """
+        if self.kind== 'input':
+            return self.val
+        else:
+            x = self.activate()
+            return (x * (1-x))
 
