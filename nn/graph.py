dc9e3288a9cad5bb71c9f24659f14343093199ae
Jay Butera <buterajay@gmail.com>
2013-11-28 01:09:30 -0600

    Fix indexing bugs and add debug prints

--- nn/graph.py
+++ nn/graph.py
@@ -11,15 +11,21 @@ class Graph:
         new_layer = InputLayer(num)
         self.layer_dict[new_layer.key] = new_layer
 
+        return new_layer
+
     def add_hidden_layer(self, num):
         self.num_layer += 1
         new_layer = HiddenLayer(num)
         self.layer_dict[new_layer.key] = new_layer
 
+        return new_layer
+
     def add_output_layer(self, num):
         self.num_layer += 1
         new_layer = OutputLayer(num)
         self.layer_dict[new_layer.key] = new_layer
+
+        return new_layer
 
 g = Graph()
 outputs = []
@@ -30,19 +36,20 @@ target = [(x,0) for x in range(-50,50)]
 MAX = 1
 
 inputs = g.add_input_layer(1)
-#hidden = g.add_hidden_layer(2)
+print 'input nodes: ', inputs.get_nodes()
+hidden = g.add_hidden_layer(2)
+print 'hidden nodes: ', hidden.get_nodes()
 output = g.add_output_layer(1)
+print 'output nodes: ', output.get_nodes()
 
 # To hidden
-'''
 for x, y in ((1,0), (2,0)):
-    g.add_edge(x,y)
+    hidden.add_edge(x,y)
 
 # To output
 for x, y in ((3, 1), (3, 2)):
-    g.add_edge(x, y)
-'''
+    output.add_edge(x, y)
 
-output.add_edge(3, 0)
+#output.add_edge(1, 0)
 
 backProp_test(inputs, output, target, MAX)
